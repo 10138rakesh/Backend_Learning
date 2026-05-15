01131d8f2c58b76bf1cb328cedf0ab19542b7250
rakesh <tripathy10138@gmail.com>
2026-05-15 22:33:24 +0530

    updated Backend_Learning project

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-
+const port=4000
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
 });
+
+
+
+//*****   .env file
+// .env file stores important project settings and secret data like PORT, API keys, and passwords 
+// separately from the main code.
+// dotenv.config() loads those values into process.env, so the project can use them securely and easily.
